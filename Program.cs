a3063d8c700ce2e41adc270f544af0ecc18e3065
Vadim <vadim-raz@yandex.ru>
2023-09-25 22:40:06 +0300

    2nd commit

--- Program.cs
+++ Program.cs
@@ -24,3 +24,49 @@ for (int i = 0; i < matrix.Length; i++)
 }
 
 
+string[] GetArrayLimitLength(string[] arr)
+{
+    string[] res = new string[arr.Length];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+            {
+                res[j] = arr[i];
+                j++;
+            }
+    }
+    return res;
+}
+
+
+int GetLengthArray(string[] arr)
+{
+    int cnt = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != null) cnt++;
+    }
+    return cnt;
+}
+
+
+string[] GetResArray(string[] arr, int length)
+{
+    string[] res = new string[length];
+    for (int i = 0; i < length; i++) 
+    {
+        res[i] = arr[i];
+    }
+    return res;
+}
+
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Write($"{arr[i]}");
+        if (i != arr.Length - 1) Write(", ");
+    }
+}
