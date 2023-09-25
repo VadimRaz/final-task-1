3b82137ebdada3a91e3aed8a63e4bce00ec450cf
Vadim <vadim-raz@yandex.ru>
2023-09-25 22:39:05 +0300

    1st commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+string[] array1 = {"hello", "2", "world", ":-)"};
+string[] array2 = {"1234", "1567", "-2", "computer science"};
+string[] array3 = {"Russia", "Denmark", "Kazan"};
+
+string[][] matrix = new string[][] {array1, array2, array3};
+
+
+for (int i = 0; i < matrix.Length; i++)
+{
+    Write("[");
+    PrintArray(matrix[i]);
+    Write("]");
+
+    Write(" -> ");
+
+    Write("[");
+    int length_array = GetLengthArray(GetArrayLimitLength(matrix[i]));
+    PrintArray(GetResArray(GetArrayLimitLength(matrix[i]), length_array));
+    WriteLine("]");
+}
+
+
